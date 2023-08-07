92c321bc16eb184fea98cd28ea434ec5cb18af3d
igwemiracle <igwemiracle35@gmail.com>
2023-08-07 22:55:17 +0100

    database connection

--- planner/database/connection.py
+++ planner/database/connection.py
@@ -1,13 +1,21 @@
+import logging
 from models.users import User
 from models.events import Event
 import motor
 import motor.motor_asyncio
 import beanie
+import logging
+
+
+
+def secret_key() -> bytes:
+    SECRET_KEY = b"HI5HL3V3L$3CR3T"
+    return SECRET_KEY
 
 # Initializes my database
-
-
 async def initialize():
     client = motor.motor_asyncio.AsyncIOMotorClient(
         "mongodb://localhost:27017/")
-    await beanie.init_beanie(database=client.db_name, document_models=[Event, User])
+    db_name = "user"
+    await beanie.init_beanie(database=client[db_name], document_models=[Event, User])
+    logging.info(f"Connected to database: {db_name}")
