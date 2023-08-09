cb0242ae6313a71ef4dda2879d70504f67e65f91
igwemiracle <igwemiracle35@gmail.com>
2023-08-09 12:02:56 +0100

    models folder

--- planner/models/events.py
+++ planner/models/events.py
@@ -1,17 +1,19 @@
 from beanie import Document
-from typing import Optional, List
-from pydantic import BaseModel, Field
+from typing import  List, Optional
+from pydantic import  Field
 from bson import ObjectId
 from beanie import PydanticObjectId
 
 
 class Event(Document):
     id: PydanticObjectId = ObjectId
+    creator:str
     title: str = Field(max_length=100)
     image: str
     description: str = Field(max_length=100)
     tags: List[str]
     location: str = Field(max_length=100)
+    
 
     class Settings:
         name = "events"
@@ -26,20 +28,3 @@ class Event(Document):
                 "location": "Google Meet"
             }}
 
-
-class EventUpdate(BaseModel):
-    title: Optional[str]
-    image: Optional[str]
-    description: Optional[str]
-    tags: Optional[List[str]]
-    location: Optional[str]
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "title": "FastAPI Book Launch",
-                "image": "https:// linktomyimage.com/image.png",
-                "description": "We will be discussing MongoDB",
-                "tags": ["python", "fastapi", "book", "launch"],
-                "location": "Google Meet"
-            }}
