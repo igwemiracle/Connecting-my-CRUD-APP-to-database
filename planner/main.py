7bfd49d4f19be2270221abf9939c67fb546049aa
igwemiracle <igwemiracle35@gmail.com>
2023-08-09 12:06:03 +0100

    main file

--- planner/main.py
+++ planner/main.py
@@ -17,5 +17,5 @@ async def connect():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080,
+    uvicorn.run("main:app", host="localhost", port=8080,
                 reload=True)
