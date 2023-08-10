c0eae021f0c4abe5885df1b4189eca0fb7acb715
igwemiracle <igwemiracle35@gmail.com>
2023-08-10 20:25:35 +0100

    main file

--- planner/main.py
+++ planner/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.users import user_router
 from routes.events import event_router
-from database.connection import initialize
+from database.connection import initialize_database
 import uvicorn
 app = FastAPI()
 
+
+#register origins
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register routes
 app.include_router(user_router, prefix="/user")
@@ -13,7 +24,7 @@ app.include_router(event_router, prefix="/event")
 
 @app.on_event("startup")
 async def connect():
-    await initialize()
+    await initialize_database()
 
 
 if __name__ == "__main__":
