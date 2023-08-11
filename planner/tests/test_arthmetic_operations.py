c33001d00e8c5a9b81d45e275d147a4eaa8913ca
igwemiracle <igwemiracle35@gmail.com>
2023-08-11 13:34:28 +0100

    tests folder

--- planner/tests/test_arthmetic_operations.py
+++ planner/tests/test_arthmetic_operations.py
@@ -17,7 +17,7 @@ def test_subtract() -> None:
     assert subtract(2, 5) == 3
 
 def test_multiply() -> None:
-    assert multiply(10, 10) == 10
+    assert multiply(10, 10) == 100
 
 def test_divide() -> None:
     assert divide(25, 100) == 4
