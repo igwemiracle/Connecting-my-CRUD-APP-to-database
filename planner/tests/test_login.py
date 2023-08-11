2766a60ddd02da6e7ddcce90b584138f699073c0
igwemiracle <igwemiracle35@gmail.com>
2023-08-11 13:35:31 +0100

    tests folder

--- planner/tests/test_login.py
+++ planner/tests/test_login.py
@@ -32,8 +32,6 @@ async def test_sign_user_in(default_client: httpx.AsyncClient) -> None:
     }
     # initiate the request and test the responses:
     response = await default_client.post("/user/signin", json=payload,headers=headers)
-    print(response.status_code)
-    print(response.text)
     assert response.status_code == 200
     assert response.json()["token_type"] == "Bearer"
 
